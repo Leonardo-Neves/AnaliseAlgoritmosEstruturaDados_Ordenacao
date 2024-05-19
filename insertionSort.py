7a21dd64a61baf82fec9e1799378e752dce86cab
Luana Felipe de Barros-LUANAFB <luanafb@motorola.com>
2024-05-19 16:03:21 -0300

    Adding bubble, insertion and quick sort algorithms

---
+++ insertionSort.py
@@ -0,0 +1,30 @@
+
+import random
+
+class InsertionSort:
+
+    def __init__(self, array):
+        self.array = array
+
+    def sort(self):
+        for i in range(1, len(self.array)):
+            aux = self.array[i]
+            j = i - 1
+            
+            while((j >= 0) and (aux < self.array[j])):
+                self.array[j+1] = self.array[j]
+                j -= 1
+            self.array[j+1] = aux
+        
+        return self.array
+            
+if __name__ == "__main__":
+    n = 20
+    itens = [random.randint(1, 100) for i in range(0, n)]
+
+    print('itens', itens)
+
+    sort_algorithm = InsertionSort(itens)
+    itens_sorted = sort_algorithm.sort()
+
+    print('itens_sorted', itens_sorted)
